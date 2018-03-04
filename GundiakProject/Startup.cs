5a01857730af438fd8bdc1fb026ed75f4177ce27
Oleg Vintoniak <olehstrong@mail.ua>
2018-03-04 12:26:12 +0200

    Add project files.

---
+++ GundiakProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GundiakProject.Startup))]
+namespace GundiakProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
